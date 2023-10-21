53adfa24a53fa9e7ee456e7ab89286bb21a5a8f8
Connor Fech <cfech44@gmail.com>
2023-10-21 14:26:20 -0400

    updating names

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Labrador labrador = new Labrador("Buddy", "Labrador Retriever", 3, "Yellow");
-        Poodle poodle = new Poodle("Lucky", "Standard Poodle", 2, "White");
+        Labrador buddy = new Labrador("Buddy", "Labrador Retriever", 3, "Yellow");
+        Poodle lucky = new Poodle("Lucky", "Standard Poodle", 2, "White");
 
-        labrador.speak();
-        labrador.eat("kibble");
-        System.out.println(labrador.info());
+        buddy.speak();
+        buddy.eat("kibble");
+        System.out.println(buddy.info());
 
-        poodle.speak();
-        poodle.eat("treats");
-        System.out.println(poodle.info());
+        lucky.speak();
+        lucky.eat("treats");
+        System.out.println(lucky.info());
     }
 }
 
